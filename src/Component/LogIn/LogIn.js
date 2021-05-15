0a58704c11098a365f0514bec7f64188ee7f03dc
Vu Cat <vucat12@gmail.com>
2021-05-16 01:49:54 +0700

    add log in

---
+++ src/Component/LogIn/LogIn.js
@@ -0,0 +1,69 @@
+
+import React from 'react';
+import './LogIn.scss';
+import { Form, Input, Button } from "antd";
+
+
+
+export const LogIn = () => {
+    const onFinish = (values) => {
+        console.log("Success:", values);
+      };
+    
+      const onFinishFailed = (errorInfo) => {
+        console.log("Failed:", errorInfo);
+      };
+   
+    return (
+        <div className="log-in">
+            <div className="background-login log-in-background"></div>
+            <div className="log-in-form">
+            <div className="log-in-title">ISOMORPHIC</div>
+                <Form
+            className="pt-4"
+                name="basic"
+                initialValues={{
+                    remember: true
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                >
+                <Form.Item
+                    name="username"
+                    rules={[
+                    {
+                        required: true,
+                        message: "Please input your username!"
+                    }
+                    ]}
+                >
+                    <Input placeholder="Username"/>
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    rules={[
+                    {
+                        required: true,
+                        message: "Please input your password!"
+                    }
+                    ]}
+                >
+                    <Input placeholder="Password"/>
+                </Form.Item>
+
+                <Form.Item className="pt-2">
+                    <Button type="primary" htmlType="submit">
+                    Sign in
+                    </Button>
+                </Form.Item>
+                </Form>
+            <div className="log-in-description">
+            username: demo password: demodemo or just click on any button.
+            </div>
+            </div>
+            
+        </div>
+    )
+}
+     
